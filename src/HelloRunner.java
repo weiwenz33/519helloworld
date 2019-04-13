8cb388f3ddba9232205d6ecc427ba94474a44809
weiwenz33 <weiwenz@seas.upenn.edu>
2019-04-13 00:41:37 -0700

    Weiwen third edit, added another println

--- src/HelloRunner.java
+++ src/HelloRunner.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
 // First edit	
 //Second edit by weiwen	
 	System.out.println("Hello World!");
+	System.out.println("This is a good day"); // Weiwen Third Edit
 	
 }
 }
